29de1e1dba41341eacfce19d9082e5a17bc7f4eb
Michael Carey <kb7ppb@hotmail.com>
2017-01-11 22:45:20 -0700

    Completed Teach 02 Assignment.

---
+++ web/scripts/teach02.js
@@ -0,0 +1,14 @@
+/*
+	Teach 02 Assignment Vanilla JavaScript File
+	Author: Michael Carey
+*/
+
+function quickAlert()
+{
+	alert('Clicked!');
+}
+
+function changeColor()
+{
+	document.getElementById("jsDiv1").style.backgroundColor = document.getElementById("colorName").value;
+}
