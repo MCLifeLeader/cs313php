81f7e0d6512caf8e55791dd580b09c7e9bf0e4b3
Michael Carey <kb7ppb@hotmail.com>
2017-02-08 22:51:11 -0700

    Removing Debuggy from file related to figuring out the Heroku vendor path issue

--- web/phpProject/bootstrap.php
+++ web/phpProject/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-	echo "<br/><br/><br/><br/><br/>";
 
 // Include the composer Autoloader
 // The location of your project's vendor autoloader.
@@ -8,7 +7,6 @@ if (!file_exists($composerAutoload)) {
     //If the project is used as its own project, it would use rest-api-sdk-php composer autoloader.
     $composerAutoload = dirname(__DIR__) . '/vendor/autoload.php';
     if (!file_exists($composerAutoload)) {
-        echo $composerAutoload;
         echo "The 'vendor' folder is missing. You must run 'composer update' to resolve application dependencies.\nPlease see the README for more information.\n";
         exit(1);
     }
