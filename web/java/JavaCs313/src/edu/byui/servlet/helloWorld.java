8d5ce79c0dff0973f5dc093dd63ea7672ff568c0
Michael Carey <kb7ppb@hotmail.com>
2017-02-21 09:25:16 -0700

    Setting up Project Stub for CS 313 class

---
+++ web/java/JavaCs313/src/edu/byui/servlet/helloWorld.java
@@ -0,0 +1,57 @@
+package edu.byui.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class helloWorld
+ */
+@WebServlet("/helloWorld")
+public class helloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public helloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		StringBuilder sb = new StringBuilder();
+		
+
+		sb.append("<!DOCTYPE html>");
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
+		sb.append("<title>Hello World</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("<h1>Hello World Servlet</h1>");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		response.getWriter().append(sb.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
